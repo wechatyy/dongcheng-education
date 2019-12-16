a7cb352fa5cfcb9c695f43faa0ad94172f751438
王亚男 <wangyanan03@renrenche.com>
2019-12-16 13:35:22 +0800

    ..

---
+++ js/public.js
@@ -0,0 +1,45 @@
+window.onload = function(){
+    // 导航
+    // var Uls = document.getElementById('uls');
+    // var Ulis = Uls.getElementsByTagName('li');
+    // var i, j;
+    // for (var i = 0; i < Ulis.length; i++) {
+    //     Ulis[i].index = i;
+    //     Ulis[i].onclick = function () {
+    //         for (j = 0; j < Ulis.length; j++) {
+    //             Ulis[j].className = "";
+    //         }
+    //         this.className = "active";
+    //     }
+    // }    
+
+    // 东方视频
+    var videoNav = document.getElementById('video-nav');
+    var videoNavItem = document.getElementById('video-nav-item');
+    videoNav.onmouseover = function () {
+        videoNavItem.style.display = "block";
+    };
+    videoNav.onmouseout = function () {
+        videoNavItem.style.display = "none";
+    }
+
+    // 图片故事
+    var storyNav = document.getElementById('story-nav');
+    var storyNavItem = document.getElementById('story-nav-item');
+    storyNav.onmouseover = function () {
+        storyNavItem.style.display = "block";
+    };
+    storyNav.onmouseout = function () {
+        storyNavItem.style.display = "none";
+    }
+
+    // 媒介素养-下载中心
+    var nav_item=document.getElementById('nav_item');
+    var nav_two=document.getElementById('nav_two');
+    nav_item.onmouseover=function (){
+        nav_two.style.display = "block";
+    };
+    nav_item.onmouseout = function(){
+        nav_two.style.display = "none";
+    }
+}
